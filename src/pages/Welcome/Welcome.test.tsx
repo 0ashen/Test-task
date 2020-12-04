d70ec0a54875063e2b93daf695d45eed66867c5d
0ashen <iusfilipp@gmail.com>
2020-12-04 03:21:50 +0300

    add two tests and mobile layout

--- src/pages/Welcome/Welcome.test.tsx
+++ src/pages/Welcome/Welcome.test.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { render, screen } from '@testing-library/react'
 import { BrowserRouter } from 'react-router-dom'
-import { App } from '../src/App/App'
+import { App } from '../../App/App'
 
 test('test render users overview page', () => {
     render(
